051fe3f1cbc3f484e8f300388c6df41e390c1cdd
Alexis <abosval95@gmail.com>
2021-11-24 12:23:35 +0100

    Ajout objet main

--- NintendoProject/NintendoProject/src/test/java/test/Test.java
+++ NintendoProject/NintendoProject/src/test/java/test/Test.java
@@ -1,10 +1,24 @@
 package test;
 
+
+
+
+
+import metier.Console;
+import metier.Jeu;
+
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Console c1 = new Console("Wii");
+		
+		
+		Jeu j1 = new Jeu ("Zelda", c1);
+		Jeu j2 = new Jeu ("Mario kart", c1);
+		Jeu j3 = new Jeu ("Smash", c1);
+		Jeu j4 = new Jeu ("DonkeyKong", c1);
+		Jeu j5 = new Jeu ("Pokemon", c1);
 
 	}
-
+	
 }
